f7e6c8a4ce3c9729a1c37af18d4dcbd20b276f8e
Carlos Eduardo Prezzi <carlosedprezzi@gmail.com>
2023-05-26 19:34:45 -0300

    Salvando alterações para ajustar em outro computador, RR em andamento

--- Scheduler/schedule_rr.c
+++ Scheduler/schedule_rr.c
@@ -1,11 +1,42 @@
 #include "schedule_rr.h"
+#include "list.h"
+#include "task.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "CPU.h"
+
+struct node *stack;
+int quantum = 2;
 
 // add a task to the list 
 void add(char *name, int priority, int burst){
-   int x = 0;
+  struct task *task = malloc(sizeof(struct task));
+  task->name = name;
+  task->priority = priority;
+  task->burst = burst;
+
+  push(&stack, task);
 }
 
 // invoke the scheduler
 void schedule(){
-   int x = 0;
+  while (stack->next) {
+    if (stack->task->burst > quantum) {
+
+      printf("a\n");    
+      run(stack->task, quantum);
+      printf("a\n");
+      printf("%lu\n", stack->task->burst);
+      stack->task->burst -= quantum;
+      printf("a\n");    
+      push(&stack, stack->task);
+      printf("a\n");    
+      stack = stack->next;
+      printf("a\n");    
+    } else {
+      run(stack->task, stack->task->burst);
+      printf("b\n");    
+    }
+  }  
 }
